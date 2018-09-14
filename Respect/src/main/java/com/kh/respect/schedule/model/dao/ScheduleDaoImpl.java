301331e126329eccefce6202d371cb5584b4f29e
acll8029 <acll8029@naver.com>
2018-09-14 15:32:53 +0900

    일정만들기 완성

--- Respect/src/main/java/com/kh/respect/schedule/model/dao/ScheduleDaoImpl.java
+++ Respect/src/main/java/com/kh/respect/schedule/model/dao/ScheduleDaoImpl.java
@@ -1,5 +1,24 @@
 package com.kh.respect.schedule.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.respect.schedule.model.vo.Schedule;
+import com.kh.respect.schedule.model.vo.TimeTable;
+
+@Repository
 public class ScheduleDaoImpl implements ScheduleDao {
+
+	@Override
+	public int insertSchedule(SqlSessionTemplate session, Schedule sc) {
+		System.out.println("스케줄 dao");
+		return session.insert("schedule.insertSchedule",sc);
+	}
+
+	@Override
+	public int insertTimeTable(SqlSessionTemplate session, TimeTable tt) {
+		System.out.println("타임테이블 dao");
+		return session.insert("schedule.insertTimeTable",tt);
+	}
 
 }
